c5d31dd45f9360d979b7eb5b19891a632154c614
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2024-08-23 09:21:25 -0300

    Create route.ts

---
+++ src/app/api/contato/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server"
+
+export async function GET() {
+    const vCardData = `BEGIN:VCARD
+VERSION:3.0
+FN:José Henrique Salvio
+ORG:CEO – FOUNDER
+TEL;TYPE=CELL:+55 31 99851-8916
+TEL;TYPE=WORK,VOICE:+55 28 3344-1022
+EMAIL:josehenrique@salviopartner.com
+URL;type=WORK:www.salviopartner.com
+NOTE:Instagram: @salviopartner
+END:VCARD`
+
+    const headers = new Headers({
+        'Content-Type': 'text/vcard',
+        'Content-Disposition': 'attachment; filename="contact.vcf"',
+    })
+
+    return new NextResponse(vCardData, { headers })
+}
